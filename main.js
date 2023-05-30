fa8244c5b9ad64c9e2ce0a14904a40ddd76f0755
Barbariskaa <129290831+Barbariskaa@users.noreply.github.com>
2023-05-30 17:58:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
-const https = require('https');
+const axios = require('axios');
 const http = require('http');
-const puppeteer = require('puppeteer');
 
+const endpointPlaceholder = 'https://api.together.xyz/api/inference';
 const COOKIE = ``;
+const headersPlaceholder = {
+    'Content-Type': 'application/json',
+    'cookie': COOKIE,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50'
+  };
 
-const options = {
-    hostname: 'play.vercel.ai',
-    port: 443,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-};
-
-function insertRandomCharacter(str) {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+[]{}|;':\",./<>?";
-    const randomIndex = Math.floor(Math.random()* characters.length);
-    const randomCharacter = characters.charAt(randomIndex);
-  
-    const insertionIndex = Math.floor(Math.random() * (str.length + 1));
-    return str.slice(0, insertionIndex) + randomCharacter + str.slice(insertionIndex);
-}
-
-const readBody = (res, json, onData) => new Promise((resolve, reject) => {
+  const readBody = (res, json, onData) => new Promise((resolve, reject) => {
     let buffer = '';
 
     res.on('data', chunk => {
@@ -41,28 +28,11 @@ const readBody = (res, json, onData) => new Promise((resolve, reject) => {
     });
 })
 
-const request = (path, data, encoding, onData) =>
-    new Promise((resolve, reject) => {
-        options.headers['User-Agent'] = `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/${Math.floor(Math.random() * 10000000)} Firefox/${(Math.random() * 200).toFixed(2)}`;
-        options.headers['Cookie'] = `user_session=${COOKIE.trim()}`;
-        options.headers['Custom-Encoding'] = encoding;
-        const req = https.request({ ...options, path }, async (res) => {
-            try {
-                const body = await readBody(res, false, onData);
-                resolve(body);
-            } catch (e) {
-                reject(e);
-            }
-        });
-
-        req.write(JSON.stringify(data));
-        req.end();
-    });
-
-async function generate(text, { model, temperature, topP, maxTokens, frequencyPenalty, presencePenalty, onData }, encoding) {
-    console.log(`Model: ${model}\nPrompt length: ${text.length}`);
+  async function generate({ prompt, prompt_format_string, top_p, temperature, max_tokens, frequencyPenalty, presencePenalty, onData }) {
+    console.log(`Prompt length: ${(prompt+prompt_format_string).length}`);
 
     let currentLine = '';
+    let firstBrokenChunk = '';
 
     let wasTimeout = false;
     let timeout;
@@ -74,236 +44,160 @@ async function generate(text, { model, temperature, topP, maxTokens, frequencyPe
         }, 15000);
     })
 
-    await Promise.race([
-        request('/api/generate', {
-            prompt: text,
-            model,
+    const apiResponse = new Promise(async resolve => {
+        const requestData = {
+            max_tokens,
+            stop:["<human>","</s>"],
+            top_p,
+            //top_k:40,
+            repetition_penalty:presencePenalty,
             temperature,
-            maxTokens: Math.min(maxTokens, 511),
-            topP,
-            frequencyPenalty,
-            presencePenalty,
-            stopSequences: model.startsWith('anthropic:claude') ? ['\nHuman:'] : [],
-        }, encoding, 
-        (line) => {
-            if (wasTimeout) return;
+            model:"sambanovasystems/BLOOMChat-176B-v1",
+            prompt,
+            prompt_format_string,
+            stream_tokens:true,
+            repetitive_penalty:frequencyPenalty
+        };
+        
+        const responseStream = await axios.default({
+            method: 'post',
+            url: endpointPlaceholder,
+            headers: {
+              ...headersPlaceholder
+            },
+            data: requestData,
+            responseType: 'stream',
+        });
+        
+        responseStream.data.on('data', (chunk) => {
+            chunk = chunk.toString().trim()
+            try{
+                if (wasTimeout) resolve();;
 
-            if (timeout) {
-                process.stdout.write('Generating response ');
-                clearTimeout(timeout);
-                timeout = 0;
-            } else {
-                process.stdout.write('.');
-            }
-            line = ((l) => {
-                try {
-                    return JSON.parse(l);
-                } catch (e) {
-                    return l;
+                if (timeout) {
+                    process.stdout.write('Generating response ');
+                    clearTimeout(timeout);
+                    timeout = 0;
+                } else {
+                    process.stdout.write('.');
                 }
-            })(line);
 
-            if (model.startsWith('anthropic:claude') && line.trim()) {
-                onData?.(line.slice(currentLine.length));
-                currentLine = line;
-            } else {
-                onData?.(line);
-                currentLine += line;
+                if(firstBrokenChunk) {
+                    chunk = firstBrokenChunk + chunk
+                    firstBrokenChunk = ''
+                }
+
+                if(chunk.includes("\n\n") && chunk.length>5) {
+                    chunks = chunk.split("\n\n")
+                    chunks.forEach(element => {
+                        element=element.slice(6)
+                        currentLine += JSON.parse(element).choices[0].text
+                        onData(JSON.parse(element).choices[0].text)
+                    });
+                }
+                else if(!chunk.endsWith("}")) {
+                    firstBrokenChunk += chunk
+                }
+                else {
+                    chunk=chunk.slice(6)
+                    currentLine += JSON.parse(chunk).choices[0].text
+                    onData(JSON.parse(chunk).choices[0].text)
+                }
+            } catch(e) {
+                onData(chunk)
             }
-        }),
-        timeoutPromise,
+        });
+        
+        responseStream.data.on('end', () => {
+            resolve()
+        });
+
+        responseStream.data.on('error', (error) => {
+            console.log("Error:",error)
+            reject(error);
+        });
+    })
+
+    await Promise.race([
+        apiResponse,
+        timeoutPromise
     ]);
 
-    console.log(wasTimeout ? 'Timeout' : ' Done');
+    console.log(wasTimeout ? 'Timeout' : 'Done');
 
     return currentLine;
 }
 
 function preparePrompt(messages) {
-    return messages.filter(m => m.content?.trim()).map(m => {
+    return messages.slice(0, -1).filter(m => m.content?.trim()).map(m => {
         let author = '';
         switch (m.role) {
-            case 'user': author = 'Human'; break;
-            case 'assistant': author = 'Assistant'; break;
-            case 'system': author = 'System Note'; break;
+            case 'user': author = '<human>'; break;
+            case 'assistant': author = '<bot>'; break;
+            case 'system': author = '<system>'; break;
             default: author = m.role; break;
         }
 
         return `${author}: ${m.content.trim()}`;
-    }).join('\n') + `\nAssistant: `;
-}
-
-async function getEncoding(updateEncodingJsonCallback) {
-    try {
-        const options = {
-            hostname: 'play.vercel.ai',
-            path: '/openai.jpeg',
-            method: 'GET',
-            headers: {
-                'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/${Math.floor(Math.random() *10000000)} Firefox/${(Math.random()* 200).toFixed(2)}`,
-                'Cookie': `user_session=${COOKIE.trim()}`
-            }
-        };
-
-        const request = https.request(options, (response) => {
-            let data = "";
-
-            response.on('data', (chunk) => {
-                data += chunk.toString()
-            });
-
-            response.on('end', () => {
-                const body = JSON.parse(Buffer.from(data, 'base64').toString('utf8'));
-                const codeFunction = eval(`(${body.c})`);
-                const result = codeFunction(body.a);
-                r_key = result[0];
-                if (isNaN(result[0])) r_key = null;
-
-                const encodingJson = {
-                    "r": [r_key, [], "sentinel"],
-                    "t": body.t
-                };
-
-                updateEncodingJsonCallback(encodingJson);
-            });
-        });
-
-        request.on('error', (error) => {
-            console.error(error);
-        });
-
-        request.end();
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function connectToVercel(updateEncodingJsonCallback) {
-    const browser = await puppeteer.launch({
-        headless: 'new'
-    })
-    const page = await browser.newPage();
-
-    page.on('response', async response => {
-        if (response.url() == "https://play.vercel.ai/openai.jpeg") {
-            const base64String = await response.text()
-            const body = JSON.parse(Buffer.from(base64String, 'base64').toString('utf8'))
-            const codeFunction = eval(`(${body.c})`);
-            const result = codeFunction(body.a);
-            r_key = result[0]
-            if(isNaN(result[0])) r_key = null
-            const encodingJson = {
-                "r":[r_key,[],"sentinel"],
-                "t":body.t
-            }
-            updateEncodingJsonCallback(encodingJson)
-        }
-    });
-
-    // Define the cookie you want to inject
-    const cookie = {
-        "domain": "play.vercel.ai",
-        "expirationDate": 2000000000,
-        "hostOnly": true,
-        "httpOnly": true,
-        "name": "user_session",
-        "path": "/",
-        "sameSite": "None",
-        "secure": true,
-        "session": false,
-        "storeId": null,
-        "value": COOKIE.trim()
-    };
-
-    // Inject the cookie into the page
-    await page.setCookie(cookie);
-
-    await page.goto('https://play.vercel.ai/');
-
-    //await browser.close();
+    }).join('\n') + (messages[messages.length - 1].role == 'user' ? `\n<human>: {prompt}\n<bot>:` :
+        messages[messages.length - 1].role == 'assistant' ? `\n<bot>: {prompt}\n<bot>:` : `\n<${messages[messages.length - 1].role}>: {prompt}\n<bot>:`)
 }
 
 async function main() {
-    let encodingJson = {}
-
-    const waitForEncodingJson = () => new Promise(resolve => {
-        const checkEncodingJson = () => {
-            if (Object.keys(encodingJson).length === 0) {
-                setTimeout(checkEncodingJson, 100);
-            } else {
-                setTimeout(resolve, 2000);
-            }
-        }
-        checkEncodingJson();
-    });
-
-    function updateJson(newEncodingJson) {
-        encodingJson = newEncodingJson
-    }
-
     const server = http.createServer(async (req, res) => {
         if (req.method.toUpperCase() === 'POST') {
-            try {
-                if (Object.keys(encodingJson).length === 0) {
-                    console.log("Ждем прогрузки vercel.ai")
-                    await waitForEncodingJson();
-                }
+            const body = await readBody(req, true);
+            const {
+                messages,
+                temperature,
+                max_tokens,
+                presence_penalty,
+                frequency_penalty,
+                top_p,
+                stream,
+            } = body;
 
-                const body = await readBody(req, true);
-                const [, owner, modelName] = req.url.split('/');
-                const model = `${owner}:${modelName}`;
+            if (stream) {
+                res.setHeader('Content-Type', 'text/event-stream');
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+            }
 
-                const jsonString = JSON.stringify(encodingJson);
-                const encoding = Buffer.from(jsonString, 'utf8').toString('base64');
+            const id = `chatcmpl-${(Math.random().toString(36).slice(2))}`;
+            const created = Math.floor(Date.now() / 1000);
 
-                const {
-                    messages,
-                    temperature,
-                    top_p,
-                    max_tokens,
-                    presence_penalty,
-                    frequency_penalty,
-                    stream,
-                } = body;
+            if (stream) {
+                const data = JSON.stringify({
+                    id, created,
+                    object: 'chat.completion.chunk',
+                    model: 'BLOOMChat-176B-v1',
+                    choices: [{
+                        delta: { role: 'assistant' },
+                        finish_reason: null,
+                        index: 0,
+                    }],
+                });
+                res.write(`data: ${data}\n\n`);
+            }
 
-                if (stream) {
-                    res.setHeader('Content-Type', 'text/event-stream');
-                } else {
-                    res.setHeader('Content-Type', 'application/json');
-                }
+            const prompt_format_string = preparePrompt(messages)
+            const prompt = messages[messages.length-1].content
 
-                const id = `chatcmpl-${(Math.random().toString(36).slice(2))}`;
-                const created = Math.floor(Date.now() / 1000);
-
-                if (stream) {
-                    const data = JSON.stringify({
-                        id, created,
-                        object: 'chat.completion.chunk',
-                        model: modelName,
-                        choices: [{
-                            delta: { role: 'assistant' },
-                            finish_reason: null,
-                            index: 0,
-                        }],
-                    });
-                    res.write(`data: ${data}\n\n`);
-                }
-
-                let prompt = preparePrompt(messages);
-                prompt = insertRandomCharacter(prompt)
-                const result = await generate(prompt, {
-                    model,
-                    temperature,
-                    topP: top_p,
-                    maxTokens: max_tokens,
-                    frequencyPenalty: frequency_penalty,
-                    presencePenalty: presence_penalty,
+            const result = await generate(
+                {
+                    temperature, 
+                    max_tokens, 
+                    presence_penalty, 
+                    frequency_penalty, 
+                    top_p, 
+                    prompt_format_string, 
+                    prompt,
                     onData: (line) => {
                         if (stream) {
                             const data = JSON.stringify({
                                 id, created,
                                 object: 'chat.completion.chunk',
-                                model: modelName,
+                                model: 'BLOOMChat-176B-v1',
                                 choices: [{
                                     delta: { content: line },
                                     finish_reason: null,
@@ -312,61 +206,53 @@ async function main() {
                             });
                             res.write(`data: ${data}\n\n`);
                         }
-                    }
-                },
-                encoding);
-
-                if (stream) {
-                    const data = JSON.stringify({
-                        id, created,
-                        object: 'chat.completion.chunk',
-                        model: modelName,
-                        choices: [{
-                            delta: {},
-                            finish_reason: 'stop',
-                            index: 0,
-                        }],
-                    });
-                    res.write(`data: ${data}\n\ndata: [DONE]\n\n`);
-                } else {
-                    res.write(JSON.stringify({
-                        id, created,
-                        object: 'chat.completion',
-                        model: modelName,
-                        choices: [{
-                            message: {
-                                role: 'assistant',
-                                content: result,
-                            },
-                            finish_reason: 'stop',
-                            index: 0,
-                        }]
-                    }));
+                    },
                 }
-
-                res.end();
-            } catch (err) {
-                console.error(err);
-                res.statusCode = 500;
-                res.write('Error: ' + err.message);
-                res.end();
+                )
+            
+            if (stream) {
+                const data = JSON.stringify({
+                    id, created,
+                    object: 'chat.completion.chunk',
+                    model: 'BLOOMChat-176B-v1',
+                    choices: [{
+                        delta: {},
+                        finish_reason: 'stop',
+                        index: 0,
+                    }],
+                });
+                res.write(`data: ${data}\n\ndata: [DONE]\n\n`);
+            } else {
+                res.write(JSON.stringify({
+                    id, created,
+                    object: 'chat.completion',
+                    model: 'BLOOMChat-176B-v1',
+                    choices: [{
+                        message: {
+                            role: 'assistant',
+                            content: result,
+                        },
+                        finish_reason: 'stop',
+                        index: 0,
+                    }]
+                }));
             }
+
+            res.end();
         } else {
             res.setHeader('Content-Type', 'application/json');
             res.write(JSON.stringify({
                 object: 'list',
                 data: [
-                    { id: 'claude-v1', object: 'model', created: Date.now(), owned_by: 'anthropic', permission: [], root: 'claude-v1', parent: null },
-                    { id: 'gpt-3.5-turbo', object: 'model', created: Date.now(), owned_by: 'openai', permission: [], root: 'gpt-3.5-turbo', parent: null },
+                    { id: 'BLOOMChat-176B-v1', object: 'model', created: Date.now(), owned_by: 'SambaNova', permission: [], root: 'BLOOMChat-176B-v1', parent: null },
                 ]
             }));
         }
         res.end();
     });
 
-    server.listen(5004, '0.0.0.0', async () => {
-        connectToVercel(updateJson)
-        console.log(`proxy for claude-v1: 'http://127.0.0.1:5004/anthropic/claude-v1'`);
+    server.listen(5011, '0.0.0.0', () => {
+        console.log(`proxy for BLOOMChat-176B-v1: 'http://127.0.0.1:5011/'`);
     });
 }
 
